3751f5891827364b9affbf5e5bff377c9ceee15a
kirat112 <harkiratsingh1128@gmail.com>
2024-11-30 11:39:15 +0530

    add dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
-import { Footer, Navbar, CardContainer, Discover } from "./index.js";
+import { Dashboard, Navbar } from "./index.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="font-sans bg-white">
       <Navbar />
-      <div className="px-40 py-5">
-        <Discover />
-        <CardContainer />
-        <Footer />
-      </div>
+      <Dashboard/>
     </div>
   );
 }
---
+++ client/src/Components/Dasboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import {Discover, CardContainer, Footer} from "../../index.js";
+
+const Dashboard = () => {
+  return (
+    <div className="px-40 py-5">
+        <Discover />
+        <CardContainer />
+        <Footer />
+      </div>
+  )
+}
+
+export default Dashboard
